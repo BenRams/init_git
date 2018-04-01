f290d7e5252793a0c96ba0f216292c6b0ff98e84
Ben Rams <33656459+BenRams@users.noreply.github.com>
2018-04-01 15:17:20 +0200

    go

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number=5;
+    for(int i=0;i<number;i++)
+    {
+        cout << "Hello world!" << endl;
+    }
+    return 0;
+}
